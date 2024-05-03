b84e104713fd3e26aa09e9b67dc0395d6fda2ab8
TahirHSTU <tahirtamin20@gmail.com>
2024-05-03 22:40:02 +0200

    removed some spaces

--- Sheep/src/sheep/DrawingArea.java
+++ Sheep/src/sheep/DrawingArea.java
@@ -1,4 +1,3 @@
-
 /**
  * Provides a panel which can be used for drawing.
  * 
@@ -9,22 +8,16 @@
 package sheep;
 import java.awt.*;
 import java.util.ArrayList;
-
 import javax.swing.JPanel;
 
 public class DrawingArea extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
-
 	protected void paintComponent(Graphics pen) {
 		super.paintComponent(pen);
 		Scene scene = new Scene();
 		scene.drawAt(pen, 4);
 	}
-
-
-	
-	
 }
 
  class Scene {
@@ -34,14 +27,12 @@ public class DrawingArea extends JPanel {
 	 private Sheep sheep4 = new Sheep();
 	 private ArrayList<Sheep> Sheeps;
 	 
-		public int generateRandom(int min, int max) {
+	 public int generateRandom(int min, int max) {
 			return  RandomNumber.between(min, max);
 			}
 		
-	 
 	 public Scene() {
 		 Sheeps = new ArrayList<Sheep>();
-		 
 		 Sheeps.add(sheep1);
 		 Sheeps.add(sheep2);
 		 Sheeps.add(sheep3);
@@ -57,23 +48,12 @@ public class DrawingArea extends JPanel {
 		 
 	 }
 	 
-
-	 
-	 public void drawAt(Graphics pen, int numberOfSheeps) {
-		 
+	 public void drawAt(Graphics pen, int numberOfSheeps) {	 
 		 for(int i = 0; i< numberOfSheeps; i++) {
 			 int height = generateRandom(400, 700) + i *200;
 			 int width = generateRandom(200, 500);
 			 Sheeps.get(i).drawAt(height, width, pen);
-		 }
-//		 for(int i = 0; i< numberOfSheeps; i++) {
-//			 int RandomValue = RandomNumber.between(700, 900);
-//			 int height = RandomValue + i *200;
-//			 int width = 200;
-//			 System.out.println(RandomValue);
-//			 Sheeps.get(i).drawAt(height, width, pen);
-//		 }
-		 
+		 }		 
 	 }
 	
 }
