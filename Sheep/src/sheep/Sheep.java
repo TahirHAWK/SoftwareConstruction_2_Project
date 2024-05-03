bd5a2099a61fe147caa74f0539ca54df3c63f9ff
Tonmoy-Majumder <160963557+Tonmoy-Majumder@users.noreply.github.com>
2024-05-03 22:46:58 +0200

    added convention on sheep file

--- Sheep/src/sheep/Sheep.java
+++ Sheep/src/sheep/Sheep.java
@@ -1,34 +1,37 @@
 package sheep;
+
 import java.awt.*;
 
-public class Sheep implements LocatedRectangle{
-	private Head head; 
-	private Body body;
-	
-	public Sheep() {
-		head = new Head(150, 120);
-		body = new Body();		
-		}
-	
-	public void drawAt(int x, int y, Graphics pen){	
-		body.drawAt(x, y, pen);
-		head.drawAt(x, y, pen);	
-	}
-	
-	public int width() {
-		return 150;
-	}
-	
-	public int height() {
-		return 150;
-	}
-	
-	public Point address() {
-		return new Point(100, 100);
-	}
-	
-	public void draw() {
-		
-	}
-	
+public class Sheep implements LocatedRectangle {
+    // Properties
+    private Head head;
+    private Body body;
+
+    // Constructor
+    public Sheep() {
+        head = new Head(150, 120);
+        body = new Body();
+    }
+
+    // Methods
+    public void drawAt(int x, int y, Graphics pen) {
+        body.drawAt(x, y, pen);
+        head.drawAt(x, y, pen);
+    }
+
+    public int width() {
+        return 150;
+    }
+
+    public int height() {
+        return 150;
+    }
+
+    public Point address() {
+        return new Point(100, 100);
+    }
+
+    public void draw() {
+
+    }
 }
