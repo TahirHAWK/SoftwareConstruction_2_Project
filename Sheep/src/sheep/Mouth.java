82980714d531dff0302889cd8d0f6df513a7051b
TahirHSTU <tahirtamin20@gmail.com>
2024-06-12 22:17:37 +0200

    done with mouth resizing

--- Sheep/src/sheep/Mouth.java
+++ Sheep/src/sheep/Mouth.java
@@ -12,8 +12,17 @@ public void drawAt(int left, int bottom){
 	Drawing.pen().setColor(Color.GRAY);
 //	Drawing.pen().drawLine(left+10, bottom+155, left+super.getHeight(), bottom+super.getWidth());
 	Drawing.pen().drawLine(left+10, bottom+super.getWidth(), left+super.getHeight(), bottom+super.getWidth());
+	System.out.println(super.getWidth() + " "+ super.getHeight());
 	}
 public void setSize(int size) {
 	this.size = size;
+	if(size == 1)
+	{
+		this.setHeight((int)(200 * 0.05833));
+		this.setWidth((int)( 200 * 0.26));
+	}else {
+		this.setHeight((int)(600* 0.05833));
+		this.setWidth((int)(600 * 0.26));		
+		}
 	}
 }
